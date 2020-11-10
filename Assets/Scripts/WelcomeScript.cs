8d92b46e26359e499c6327bcc79ad4e41721b217
trphuc <trphucdn@gmail.com>
2020-11-09 16:56:17 -0800

    Welcome screenn

---
+++ Assets/Scripts/WelcomeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WelcomeScript : MonoBehaviour
+{
+    public Button startBtn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = startBtn.GetComponent<Button>();
+		btn.onClick.AddListener(startedClick);   
+         }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void startedClick()
+    {
+       SceneManager.LoadScene(1);
+
+    }
+}
